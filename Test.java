01faccaaa0b711dabf43a7c9bc860b24194d2827
Junxuan <1246166624@qq.com>
2020-03-02 17:05:50 +0800

    test

---
+++ Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+	public static void main(String args[]) {
+		System.out.println("hahaº°º°hhan");
+		int i = 1;
+		for (i= 1;i<10;i++)
+		{
+			System.out.println(i);
+		}
+	}
+	
+}
